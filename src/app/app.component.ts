6e5b26f97079f4dcdfd3d60417cc8c8cd94cb3f5
FabioV <fabio.viani01@gmail.com>
2025-12-11 16:09:25 +0100

    Added top-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProductListComponent } from './features/products/product-list/product-list.component';
 import { CartComponent } from './features/cart/cart.component';
+import { TopBarComponent } from "./features/top-bar/top-bar.component";
 
 @Component({
   selector: 'app-root',
@@ -13,10 +14,11 @@ import { CartComponent } from './features/cart/cart.component';
     CartComponent,
     FormsModule,
     ReactiveFormsModule,
-  ],
+    TopBarComponent
+],
   template: `
     <div class="container mx-auto p-6">
-      <h1 class="text-3xl font-bold mb-6">Angular E-commerce Test</h1>
+      <app-top-bar></app-top-bar>
       <app-product-list></app-product-list>
       <app-cart></app-cart>
     </div>
