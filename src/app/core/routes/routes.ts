9a3527ae07c171ca132fee9d1d741d9530c9398c
fvianidst <fabio.viani01@gmail.com>
2026-01-07 10:43:08 +0100

    refactor

--- src/app/core/routes/routes.ts
+++ src/app/core/routes/routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { authGuard } from '../guards/auth-guard';
-import { guestGuard } from '../guards/guest-guard-guard';
 export const routes: Routes = [
     {path: '',loadComponent:() =>import('../../features/products/product-list/product-list.component').then(c=>c.ProductListComponent)},
     {path: 'checkout',loadComponent:() =>import('../../features/chekout/chekout.component').then(c=>c.ChekoutComponent),canActivate:[authGuard]},
